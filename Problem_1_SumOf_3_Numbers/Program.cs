a725140fb34e8ec6d48b28e954d6e05eb8f16026
VeskoMitev <vesko.mitev@gmail.com>
2015-11-22 10:48:53 +0200

    First commit

---
+++ Problem_1_SumOf_3_Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1_SumOf_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
