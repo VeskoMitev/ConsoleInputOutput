97be99c2bf61f85b9a7920efdcfcc56dc394927b
VeskoMitev <vesko.mitev@gmail.com>
2015-12-01 18:58:16 +0200

    Homework

--- Problem_13_CurrencyCheck/CurrencyCheck.cs
+++ Problem_13_CurrencyCheck/CurrencyCheck.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleInputOutput
+﻿namespace ConsoleInputOutput
 {
+    using System;
     class CurrencyCheck
     {
         static void Main(string[] args)
         {
+            decimal pricesOfGame;
+            decimal[] exchangeRates = new[] {0.035m, 1.5m, 1.95m, 0.5m, 1};
+            decimal cheapestGame =Decimal.MaxValue;
+            for (int i = 0; i < 5; i++)
+            {
+                pricesOfGame = uint.Parse(Console.ReadLine()) * exchangeRates[i];
+                if (cheapestGame > pricesOfGame) cheapestGame = pricesOfGame;
+            }
+            Console.WriteLine("{0:F2}",cheapestGame);
         }
     }
 }
